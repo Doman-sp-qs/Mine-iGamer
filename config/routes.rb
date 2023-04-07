a6208aac49ce5409f5a04e0cac92b7ca06ddd447
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-07 07:51:03 +0000

    [Add] 管理者側のコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   namespace :public do
     get 'posts/index'
     get 'posts/show'
