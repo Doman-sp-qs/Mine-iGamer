57c4d71166a88dec9f30a804b25885a6bbef5342
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-10 11:09:15 +0000

    [Fix] 管理者のログイン時のヘッダーのリンク編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -27,4 +27,17 @@ class Customer < ApplicationRecord
   # end
   
   
+  # フォローフォロワー機能
+  def follow(customer_id)
+    relationships.create(followed_id: customer_id)
+  end
+  
+  def unfollow(customer_id)
+    relationships.find_by(followed_id: customer_id).destroy
+  end
+  
+  def following?(customer)
+    followings.include?(customer)
+  end
+  
 end
