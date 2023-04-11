de403ae121cb8ac8f92c721ef75928fb68ff0998
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-11 13:50:20 +0000

    [Add] プロフィール編集機能実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+  
   def show
     @customer = Customer.find(params[:id])
     @posts = @customer.posts
@@ -6,5 +7,23 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :profile_image)
+  end
+  
+  
 end
