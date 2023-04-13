d1b57bfbc154b15b0fea7f833c99734f56197a4e
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-13 09:14:12 +0000

    [Fix] 管理者のアクセス制限実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!, except: [:top, :about]
+  before_action :authenticate_customer!, except: [:top, :about], unless: :admin_url 
+  before_action :authenticate_admin!, if: :admin_url 
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  
+  # ログイン後の遷移先
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
       # 管理者のトップページ(顧客一覧)
@@ -12,6 +15,7 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  # ログアウト後の遷移先
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :user
       # トップページ
@@ -24,13 +28,20 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  protected
+  # 管理者ページurlの取得
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
   
+  # customerユーザのis_stoppingステータスの判定
   def customer_is_stopping?
     if current_customer.is_stopping
       redirect_to new_customer_session_path, notice: "このアカウントは利用停止中です"
     end
   end
+  
+  
+  protected
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
