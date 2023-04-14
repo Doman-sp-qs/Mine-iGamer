3697a1f2207830978a874dc81cfb3a465014c689
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-14 05:32:51 +0000

    [Add] 管理者用投稿削除機能実装

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -18,6 +18,12 @@ class Admin::PostsController < ApplicationController
     end
   end
   
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
+  end
+  
   
   private
   
