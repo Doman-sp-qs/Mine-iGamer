c0a82540e09c96698c1cccac140e353e396ac88b
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-14 04:42:58 +0000

    [Fix,Add] 投稿一覧ページを管理者用に編集、管理者用投稿詳細ページ作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,12 @@
 class Admin::PostsController < ApplicationController
   def show
+    @post = Post.find(params[:id])
+    @customer = Customer.find(@post.customer_id)
+    @new_post = Post.new
+    @post_comment = PostComment.new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 end
