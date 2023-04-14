ad43a6e848f4b8cb2635dbcc9bae56adfe9ef054
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-14 04:07:58 +0000

    [Add,Fix] customerページの管理者用編集ボタンを修正、管理者用customer情報編集ページ作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -6,5 +6,24 @@ class Admin::CustomersController < ApplicationController
   end
   
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id), notice: "ユーザー情報の更新に成功しました"
+    else
+      render :edit
+    end
+  end
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :profile_image)
+  end
+  
+  
 end
