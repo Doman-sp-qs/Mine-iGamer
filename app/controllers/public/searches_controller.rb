f4227632cdc9d34d518e0700ba8827b2864ef578
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-24 10:39:12 +0000

    [Fix] 検索結果修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,11 +3,13 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_customer!
   
   def search
+    # 検索項目の定義
     @range = params[:range]
-    
-    if @range == "Customer"
+    if @range == "ユーザ名"
+      # 検索項目がユーザ名の時のデータ取得
       @customers = Customer.looks(params[:search], params[:word])
     else
+      # 検索項目がCustomer以外の時のデータ取得
       @posts = Post.looks(params[:search], params[:word])
     end  
   end
