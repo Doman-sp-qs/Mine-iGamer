8f5761bfab24c9bd402fff7aecca20b71c1f159f
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-04-25 07:58:58 +0000

    [Add] top,aboutページ作成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -7,7 +7,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     customer = Customer.guest
     sign_in customer
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
   end
   
   
